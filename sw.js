997c9253b24bc83881657817bd6f698d67e2fc88
ktdata405 <ktdata405@gmail.com>
2026-04-07 01:41:15 +0530

    web app compatible test fix

--- sw.js
+++ sw.js
@@ -1,26 +1,43 @@
-const CACHE_NAME = 'kt-apps-v2';
+const CACHE_NAME = 'kt-apps-v3';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/dashboard.html',
-  '/auth.js',
-  '/localization.js',
-  '/config.js',
-  '/root/logo.png',
-  '/cashew/cashew.html',
-  '/cashew/cashewreport.html',
-  '/cashew/cashew_google_apps_script.js',
-  '/rent/tenet.html',
-  '/msi/msi.html',
-  '/debts/debts.html',
-  '/denomination/denominations.html',
-  '/calculator/calculator.html',
-  '/loan/loan.html',
+  './',
+  './index.html',
+  './dashboard.html',
+  './auth.js',
+  './localization.js',
+  './config.js',
+  './manifest.json',
+  './root/logo.png',
+  './cashew/cashew.html',
+  './cashew/cashewreport.html',
+  './cashew/cashew_google_apps_script.js',
+  './rent/tenet.html',
+  './rent/tenetreport.html',
+  './rent/script.js',
+  './msi/msi.html',
+  './msi/msireport.html',
+  './debts/debts.html',
+  './debts/debtsreport.html',
+  './denomination/denominations.html',
+  './denomination/denominationsreport.html',
+  './calculator/calculator.html',
+  './calculator/village_interest.html',
+  './calculator/interest_float_flat.html',
+  './calculator/gold_loan.html',
+  './calculator/land.html',
+  './calculator/lamf.html',
+  './calculator/govt_schemes.html',
+  './calculator/vehicle_info.html',
+  './loan/loan.html',
+  './loan/loanreport.html',
+  './loan/script.js',
+  './loan/style.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
