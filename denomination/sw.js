babb6bdbdff8ff260d2cdaae88cc225864284ce7
ktdata405 <ktdata405@gmail.com>
2026-05-22 12:59:05 +0530

    Revert "install app"
    This reverts commit c704de0253726c85a045b57d795ea834e18f1680.

--- denomination/sw.js
+++ denomination/sw.js
@@ -1,9 +1,8 @@
-const CACHE_NAME = 'denominations-app-v2';
+const CACHE_NAME = 'denominations-app-v1';
 const STATIC_ASSETS = [
   './denominations.html',
   './denominations.html?from=standalone',
   './denominationsreport.html',
-  './install.html',
   './manifest.json',
   '../root/logo.png',
   '../config.js',
